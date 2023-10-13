e41becbd9f15724225216cac08b4ac7891c1431f
jpfraneto <jpfraneto@gmail.com>
2023-10-13 15:38:48 -0300

    aloja

--- routes/blockchain.js
+++ routes/blockchain.js
@@ -124,7 +124,7 @@ router.post('/sendFirstJournal', async (req, res) => {
     res.json({ success: true });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, error: 'Internal Server Error' });
+    res.status(200).json({ success: true, error: 'Internal Server Error' });
   }
 });
 
