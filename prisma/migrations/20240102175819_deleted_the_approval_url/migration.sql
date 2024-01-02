950cf47e628b304b38bc07fd739f9ab2793a6829
jp <jp@jps-MacBook-Air.local>
2024-01-02 14:58:35 -0300

    vamo vamo

---
+++ prisma/migrations/20240102175819_deleted_the_approval_url/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `approvalUrl` on the `FarcasterAccount` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "FarcasterAccount" DROP COLUMN "approvalUrl";
