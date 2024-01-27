2e8d7d9d6fa0106998187c3d29c4c3d299f853ee
jp <jp@jps-MacBook-Air.local>
2024-01-27 02:49:06 -0300

    aloja

--- routes/farcaster-frames.js
+++ routes/farcaster-frames.js
@@ -19,6 +19,7 @@ const client = getSSLHubRpcClient(HUB_URL);
 router.get("/", async (req, res) => {
   try {
     let randomAnkyIndex = Math.floor(88 * Math.random());
+    console.log("the random anky index is: ", randomAnkyIndex);
     res.setHeader("Content-Type", "text/html");
     res.status(200).send(`
 <!DOCTYPE html>
@@ -28,8 +29,7 @@ router.get("/", async (req, res) => {
   <meta property="og:title" content="you are it">
   <meta property="og:image" content="https://ibb.co/R7jYYBq">
   <meta name="fc:frame" content="vNext">
-  <meta name="fc:frame:image" content="https://jpfraneto.github.io/anky-images/${randomAnkyIndex}.png
-  ">
+  <meta name="fc:frame:image" content="https://jpfraneto.github.io/anky-images/${randomAnkyIndex}.png">
   <meta name="fc:frame:post_url" content="https://api.anky.lat/farcaster-frames">
   <meta name="fc:frame:button:1" content="<">
   <meta name="fc:frame:button:2" content="👽">
