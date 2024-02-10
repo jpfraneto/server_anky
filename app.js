0fa549affbab816b4413e7186f4eb0eb64c432bc
jp <jp@jps-MacBook-Air.local>
2024-02-10 08:58:04 -0300

    aloja

--- app.js
+++ app.js
@@ -68,9 +68,9 @@ app.use("/user", userRoutes);
 // scheduleReminders();
 
 // schedule.scheduleJob("*/5 * * * *", checkAndUpdateAnkys);
-// schedule.scheduleJob("*/5 * * * *", checkAndUpdateGeneratedAnkys);
+schedule.scheduleJob("*/5 * * * *", checkAndUpdateGeneratedAnkys);
 // checkAndUpdateMidjourneyOnAFrameAnkys();
-checkAndUpdateGeneratedAnkys();
+// checkAndUpdateGeneratedAnkys();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Anky Backend!");
